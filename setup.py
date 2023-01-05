50e5bbfa905985e50717b61d0f9fc82c90de7570
Martin Votruba <martin@nftscoring.com>
2023-01-05 16:17:21 +0100

    increase version of eth-account

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     install_requires=[
         "aiohttp>=3.7.4.post0,<4",
         "eth-abi>=2.0.0b6,<3.0.0",
-        "eth-account>=0.5.9,<0.6.0",
+        "eth-account>=0.5.9,<0.9.0",
         "eth-hash[pycryptodome]>=0.2.0,<1.0.0",
         # eth-account allows too broad of an eth-rlp dependency.
         # This eth-rlp pin can be removed once it gets tightened up in eth-account
